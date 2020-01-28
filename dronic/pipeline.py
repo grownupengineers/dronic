836d00e69954df2ae31c74a4b00ee22069ff6756
Tiago Teixeira <tiago.t@sapo.pt>
2020-01-28 22:08:58 +0000

    Another POC initial code

---
+++ dronic/pipeline.py
@@ -0,0 +1,31 @@
+
+
+#
+# Dronic Pipeline
+#
+class Pipeline(object):
+
+	def __init__(self):
+		self._stages = []
+
+	#
+	# stage decorator
+	#
+	def decorator(self, function):
+		self._stages.append(function)
+		return function	# or None
+
+	def run(self):
+		success = True
+		for stage in self._stages:
+			print("Stage", stage.__name__)
+			try:
+				ret_val = stage()
+				if not ret_val and ret_val is not None:
+					raise Exception("Stage returned False")
+			except Exception as e:
+				print("Caught exception:", str(e))
+				success = False
+				break
+		print("Pipeline status:", success)
+		
---
+++ poc2.py
@@ -0,0 +1,14 @@
+
+# old, see dronic package
+
+class DronicPipeline(object):
+
+	#
+	# stage decorator
+	#
+	def stage()
+
+class DronicJob(object):
+
+	def __init__(self, name: str, author: str):
+		self._stages = []
