d2651e08a56eb75e78f88c39b41374f39ca9be0c
Tiago Teixeira <tiago.t@sapo.pt>
2020-01-29 21:10:04 +0000

    error catching

--- dronic/__main__.py
+++ dronic/__main__.py
@@ -21,31 +21,38 @@ try:
     fd = open(argv[1])
     contents = fd.read()
     fd.close()
+except Exception as e:
+    print("Error reading job file:", str(e))
+    exit(1)
 
-    workspace = mkdtemp()
+workspace = mkdtemp()
 
-    pipeline = Pipeline()
-    stage = pipeline.decorator
+pipeline = Pipeline()
+stage = pipeline.decorator
 
-    safe_globals['stage'] = stage
-    # We will provide a set of API objects ( ie: credentials, git, docker, etc ) 
-    safe_globals['core'] = Core(jobfile = argv[1],argv = argv[2:],workspace = workspace)
+safe_globals['stage'] = stage
+# We will provide a set of API objects ( ie: credentials, git, docker, etc ) 
+safe_globals['core'] = Core(jobfile = argv[1],argv = argv[2:],workspace = workspace)
 
-    job_locals = {}
+job_locals = {}
 
+try:
     # The pipeline runs on a sandboxed environment with limited access
-    byte_code = compile_restricted(contents, argv[0], 'exec')
+    byte_code = compile_restricted(contents, argv[1], 'exec')
     exec(byte_code, safe_globals,job_locals)
+except Exception as e:
+    print("Error compiling job file:", str(e))
+    exit(2)
 
+try:
     pipeline.run()
+except Exception as e:
+    print("Error running job file:", str(e))
+    exit(3)
 
-    # clean temp dir, or not?
-    for root, dirs, files in os.walk(workspace, topdown=False):
-        for name in files:
-            os.remove(os.path.join(root,name))
-        for name in dirs:
-            os.rmdir(os.path.join(root, name))
-
-except:
-    print("Error opening or running pipeline")
-    pass
+# clean temp dir, or not?
+for root, dirs, files in os.walk(workspace, topdown=False):
+    for name in files:
+        os.remove(os.path.join(root,name))
+    for name in dirs:
+        os.rmdir(os.path.join(root, name))
