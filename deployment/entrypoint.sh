3e359e2bfaa1f72eb9bdcfff594f8e7f35023935
Tiago Teixeira <tiago.t@sapo.pt>
2020-02-15 19:20:12 +0000

    docker file order and entrypoint

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # make sure we're there
-test -d ${WORKSPACE} || { echo "mount jobs folder at /workspace"; exit 1 }
+test -d ${WORKSPACE} || { echo "mount jobs folder at /workspace"; exit 1; }
 cd ${WORKSPACE}
 
 export PYTHONPATH=/usr/src
