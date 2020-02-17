bdd9dae4c2ec043fe25be5ef159b18779430d33f
Tiago Teixeira <tiago.t@sapo.pt>
2020-02-17 22:07:22 +0000

    git pull and list branches
    plus some minor fixes

--- dronic/plugins/git.py
+++ dronic/plugins/git.py
@@ -64,10 +64,13 @@ class GitPlugin(Plugin):
         # like above
         repo = porcelain.Repo(location)
 
-        commit = repo.__getitem__(b'refs/remotes/origin/%s' % branch.encode())
+        commit = repo[b'refs/remotes/origin/%s' % branch.encode()]
         if commit is None:
             raise Exception(f"branch/commit {branch} not found")
         
+        # get remote
+        remote = repo.get_config()[(b'remote',b'origin')][b'url']
+
         # fetch/pull branch from remote
         porcelain.pull(
             repo.path,
@@ -75,10 +78,54 @@ class GitPlugin(Plugin):
             refspecs=[b'refs/heads/%s' % branch.encode()]
         )
 
-        # checkout/build index
-        repo.reset_index(commit.tree)
         # set head
         repo.refs.set_symbolic_ref(
             b"HEAD",
             b"refs/heads/%s" % branch.encode
         )
+        # checkout/build index
+        commit = repo[b'HEAD']
+        repo.reset_index(commit.tree)
+    
+    def pull(self, location='.'):
+        repo = porcelain.Repo(location)
+
+        # get head refspec
+        head = repo.refs.get_symrefs()[b'HEAD']
+
+        # get remote
+        remote = repo.get_config()[(b'remote',b'origin')][b'url']
+
+        porcelain.pull(
+            repo.path,
+            remote,
+            refspecs=[head]
+        )
+
+        # update index
+        commit = repo[b'HEAD']
+        repo.reset_index(commit.tree)
+
+        # done
+        
+
+    # list branches
+    @property
+    def branches(self, location='.'):
+        repo = porcelain.Repo(location)
+
+        refs = [ref.decode() for ref in repo.get_refs().keys()]
+
+        branches = []
+        for ref in refs:
+            if ref.startswith('refs/heads'):
+                branch = ref.replace('refs/heads','',1))
+            elif ref.startswith('refs/remotes/origin'):
+                branch = ref.replace('refs/remotes/origin','',1))
+            else:
+                # wierd
+                continue
+            if branch not in branches:
+                branches.append(branch)
+        
+        return branches
