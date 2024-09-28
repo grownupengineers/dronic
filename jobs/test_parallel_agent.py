0a4834b6690cc7ac1ce7ab5daad8c6027ba220c8
Tiago Teixeira <tiago.t@sapo.pt>
2024-09-28 11:07:48 +0200

    Add example pipeline using Parallel and Agent
    This is still just a guideline on how things could or would look like with
    Parallel and Agent implemented.

---
+++ jobs/test_parallel_agent.py
@@ -0,0 +1,20 @@
+
+@stage("Executed locally")
+def locally():
+	workspace.log("Executing locally")
+
+with Agent("x86_64"):
+
+	@stage("on agent")
+	def on_agent():
+		workspace.log("Executing on agent")
+
+with Parallel():
+	@stage("parallel-1")
+	def pll_1():
+		workspace.log("Executing in parallel")
+
+	@stage("parallel-2")
+	def pll_2():
+		workspace.log("Also in parallel")
+
