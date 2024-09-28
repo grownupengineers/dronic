b4ae2e61a0a130156c3b9d981db18cb3f6f04f9a
Tiago Teixeira <tiago.t@sapo.pt>
2024-09-28 19:45:54 +0200

    skeleton for Agent provider plugins

--- dronic/plugin.py
+++ dronic/plugin.py
@@ -25,4 +25,30 @@ class CredentialsPlugin(Plugin):
     def __init__(self):
         pass
 
-    
+class AgentPlugin(object):
+    """Base class for Agent Provider plugin.
+    """
+
+    def __init__(self):
+        pass
+
+    def can_provide(self, *args, **kwds) -> bool:
+        """Function called by Agent stage to ask if this plugin can provide
+        an agent, based on the arguments passed by the developer.
+        """
+        raise NotImplementedError
+
+    def provision(self, *args, **kwds) -> "multiprocessing.BaseManager":
+        """Function called by agent to ask for a new `multiprocessing.Manager`,
+        aka Agent.
+        The plugin shall provision an agent and return an already connected
+        `multiprocessing.BaseManager` (or a derivative class, TBD)
+        """
+        raise NotImplementedError
+
+    def shutdown(self, manager: "multiprocessing.BaseManager"):
+        """Function called by agent to signal that the agent is not longer
+        needed.
+        """
+        raise NotImplementedError
+
